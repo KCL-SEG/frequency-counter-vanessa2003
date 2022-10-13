1d500122df4f4468b25c216186b0a859bc859d90
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2022-10-13 19:02:27 +0100

    list of items convert to dictionary

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,13 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
+    items =  [str(i) for i in items]
+    frequencies = {i:items.count(i) for i in items}
+    
+    
     # Your code goes here
     return frequencies
+
+items = [100, 'Hello', '100', '100', 100]
+
+print(frequencies(items)) 
